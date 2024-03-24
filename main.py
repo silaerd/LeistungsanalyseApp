a4847e42455f7d9dd63f6d6e662824dfe68af8ed
silaerd <163121016+silaerd@users.noreply.github.com>
2024-03-24 14:05:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import json
 from my_functions import build_person, build_experiment
 
 def main():
@@ -18,16 +19,10 @@ def main():
     # Experiment erstellen
     experiment = build_experiment(experiment_name, date, supervisor, subject)
 
-    # Experiment ausgeben
-    print("Experiment erstellt:")
-    print("Experiment Name:", experiment['experiment_name'])
-    print("Datum:", experiment['date'])
-    print("Supervisor:", experiment['supervisor'])
-    print("Versuchsperson:")
-    print("Vorname:", experiment['subject']['first_name'])
-    print("Nachname:", experiment['subject']['last_name'])
-    print("Geschlecht:", experiment['subject']['sex'])
-    print("Alter:", experiment['subject']['age'])
+    # Speichern des Experiments in einer Datei
+    with open("experiment.json", "w") as outfile:
+        json.dump(experiment, outfile, indent=4)
+
 
 if __name__ == "__main__":
     main()
