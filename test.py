5a6a71bafbe62fac5bdaad8facea75cf29176ea8
Luggy85 <162305613+Luggy85@users.noreply.github.com>
2024-04-19 15:50:53 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,15 +1,15 @@
 from my_classes import Person, Experiment
 
-def test_case_1():
+def test_experiment1():
     # Testfall 1: Eine Person und ein Experiment erstellen und speichern
-    first_name = "Lukas"
-    last_name = "Balog"
-    sex = "male"
+    first_name = "Sila"
+    last_name = "Erdogan"
+    sex = "female"
     age = 21
 
     experiment_name = "First Experiment"
     date = "10.04.2024"
-    supervisor = "Sila"
+    supervisor = "Lukas"
 
     person = Person(first_name, last_name, age, sex)
     max_hr_bpm = person.estimate_max_hr()
@@ -18,12 +18,12 @@ def test_case_1():
     experiment.save("test_experiment.json")
     return max_hr_bpm
 
-def test_case_2():
+def test_experiment2():
     # Testfall 2: Eine andere Person und ein anderes Experiment erstellen und speichern
-    first_name = "Sila"
-    last_name = "Erdogan"
-    sex = "female"
-    age = 21
+    first_name = "Zoltan"
+    last_name = "Balog"
+    sex = "male"
+    age = 45
 
     experiment_name = "Second Experiment"
     date = "11.04.2024"
@@ -37,7 +37,7 @@ def test_case_2():
     return max_hr_bpm
 
 if __name__ == "__main__":
-    max_hr_person1 = test_case_1()
-    max_hr_person2 = test_case_2()
+    max_hr_person1 = test_experiment1()
+    max_hr_person2 = test_experiment2()
     print('Die Maximale Herzfrquenz von Person 1 beträgt:', max_hr_person1)
     print('Die Maximale Herzfrquenz von Person 1 beträgt:', max_hr_person2)
