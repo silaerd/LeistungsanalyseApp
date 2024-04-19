a3a84c3dea9e6a8b5c3f4c5897dd6acf533a5398
silaerd <163121016+silaerd@users.noreply.github.com>
2024-04-19 21:15:22 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import json
 from my_functions import build_person, build_experiment, estimate_max_hr
-from my_classes import Experiment, Person
 
 def main():
     # Benutzer nach den benötigten Daten für die Person fragen
@@ -13,21 +13,34 @@ def main():
     date = input("Enter date: ")
     supervisor = input("Enter supervisor name: ")
 
-    # Erstellen einer Instanz der Klasse Person
-    person = Person(first_name, last_name, age, sex)
-
-    # Berechnung der maximalen Herzfrequenz
-    max_hr_bpm = person.estimate_max_hr()
+    max_hr_bpm = estimate_max_hr(age, sex)
     print("Max Heart Rate (bpm):", max_hr_bpm)
 
-    # Erstellen einer Instanz der Klasse Experiment
-    experiment = Experiment(experiment_name, date, supervisor)
+    # Dictionary für die Person erstellen
+    person_info = build_person(first_name=first_name, last_name=last_name, sex=sex, age=age)
 
-    experiment.save("experiment.json")
+    # Dictionary für das Experiment erstellen
+    experiment = build_experiment(experiment_name=experiment_name, date=date, supervisor=person_info, subject=person_info)
+    
+    experiment_info = {
+        "experiment_name": experiment_name,
+        "date": date,
+        "supervisor": person_info,
+        "subject": {
+            "first_name": first_name,
+            "last_name": last_name,
+            "age": age,
+            "sex": sex
+        }
+    }
+
+    # Experimenten-Dictionary ausgeben
+    print("Experiment Information:")
+    print(experiment_info)
+
+    # Experimenten-Dictionary in einer Datei speichern
+    with open("experiment.json", "w") as outfile:
+        json.dump(experiment_info, outfile)
 
 if __name__ == "__main__":
     main()
-
-
-
-
