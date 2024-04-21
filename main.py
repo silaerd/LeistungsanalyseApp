af0bf1a2cc534d7ca1cff7983ab6b5844141c326
Luggy85 <162305613+Luggy85@users.noreply.github.com>
2024-04-21 09:28:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,28 +2,44 @@ from my_functions import build_person, build_experiment, estimate_max_hr
 from my_classes import Experiment, Person
 
 def main():
-    # Benutzer nach den benötigten Daten für die Person fragen
+    # Patienten daten anlegen
     first_name = input("Enter first name: ")
     last_name = input("Enter last name: ")
     sex = input("Enter sex: ")
     age = int(input("Enter age: "))
 
-    # Benutzer nach den benötigten Daten für das Experiment fragen
+    # Experimentendaten anlegen
     experiment_name = input("Enter experiment name: ")
     date = input("Enter date: ")
     supervisor = input("Enter supervisor name: ")
-
-    # Erstellen einer Instanz der Klasse Person
-    person = Person(first_name, last_name, age, sex)
 
     # Berechnung der maximalen Herzfrequenz
     max_hr_bpm = person.estimate_max_hr()
     print("Max Heart Rate (bpm):", max_hr_bpm)
 
-    # Erstellen einer Instanz der Klasse Experiment
-    experiment = Experiment(experiment_name, date, supervisor)
+    # Dictionary für den Patienten erstellen
+    person_info = build_person(first_name=first_name, last_name=last_name, sex=sex, age=age)
 
-    experiment.save("experiment.json")
+    # Dictionary für das Experiment erstellen
+    experiment = build_experiment(experiment_name=experiment_name, date=date, supervisor=person_info, subject=person_info)
+
+    # Dictionary für die Experiment-Informationen erstellen
+    experiment_info = {
+        "experiment_name": experiment_name,
+        "date": date,
+        "supervisor": supervisor,
+        "first_name": first_name,
+        "last_name": last_name,
+        "age": age,
+        "sex": sex
+        }
+
+    # Experimenten-Information ausgeben
+    print("Experiment Information:", experiment_info)
+    
+    # Experimenten-Information in einer Datei speichern
+    with open("experiment.json", "w") as outfile:
+        json.dump(experiment_info, outfile)
 
 if __name__ == "__main__":
     main()
