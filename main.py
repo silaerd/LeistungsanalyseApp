af3391de625632c98b424af46301f142a30052b9
silaerd <163121016+silaerd@users.noreply.github.com>
2024-04-24 19:54:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,44 +1,47 @@
 import json
-from my_functions import build_person, build_experiment, estimate_max_hr
+from my_functions import build_person, build_experiment
+from my_classes import Subject
 
 def main():
     # Benutzer nach den benötigten Daten für die Person fragen
     first_name = input("Enter first name: ")
     last_name = input("Enter last name: ")
     sex = input("Enter sex: ")
-    age = int(input("Enter age: "))
+    birth_date = input("Enter birth date (YYYY-MM-DD): ")
 
-    # Benutzer nach den benötigten Daten für das Experiment fragen
+    # Instanz für die Person erstellen
+    person = Subject(first_name, last_name, birth_date, sex)
+
+    # Maximale Herzfrequenz schätzen
+    max_hr_bpm = person.estimate_max_hr()
+    print("Max Heart Rate (bpm):", max_hr_bpm)
+
+    # Instanz für das Experiment erstellen
     experiment_name = input("Enter experiment name: ")
     date = input("Enter date: ")
     supervisor = input("Enter supervisor name: ")
-
-    max_hr_bpm = estimate_max_hr(age, sex)
-    print("Max Heart Rate (bpm):", max_hr_bpm)
-
-    # Dictionary für die Person erstellen
-    person_info = build_person(first_name=first_name, last_name=last_name, sex=sex, age=age)
-
-    # Dictionary für das Experiment erstellen
-    experiment = build_experiment(experiment_name=experiment_name, date=date, supervisor=person_info, subject=person_info)
+    experiment = build_experiment(experiment_name, date, supervisor)
 
     # Dictionary für die Experiment-Informationen erstellen
     experiment_info = {
         "experiment_name": experiment_name,
         "date": date,
         "supervisor": supervisor,
-        "first_name": first_name,
-        "last_name": last_name,
-        "age": age,
-        "sex": sex
+        "subject": {
+            "first_name": first_name,
+            "last_name": last_name,
+            "birth_date": birth_date,
+            "sex": sex,
+            "max_hr_bpm": max_hr_bpm
         }
+    }
 
     # Experimenten-Dictionary ausgeben
     print("Experiment Information:", experiment_info)
 
     # Experimenten-Dictionary in einer Datei speichern
     with open("experiment.json", "w") as outfile:
-        json.dump(experiment_info, outfile)
+        json.dump(experiment_info, outfile, indent=4)
 
 if __name__ == "__main__":
     main()
