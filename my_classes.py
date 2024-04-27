dc61f8a627fba55ab383a43384f8998d77a9809f
Luggy85 <162305613+Luggy85@users.noreply.github.com>
2024-04-27 09:55:42 +0200

    Update my_classes.py

--- my_classes.py
+++ my_classes.py
@@ -11,17 +11,24 @@ class Person:
         data = self.__dict__
         with open(filename, 'w') as file:
             json.dump(data, file)
+    
+    def dictionary(self):
+        return {
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'sex': self.sex,
+            'age': self.age
+        }
 
+class Subject(Person):
     def estimate_max_hr(self):
         if self.sex == "male":
-            max_hr_bpm =  223 - 0.9 * self.age
+            return 223 - 0.9 * self.age
         elif self.sex == "female":
-             max_hr_bpm = 226 - 1.0 *  self.age
+            return 226 - 1.0 * self.age
         else:
-        # der input() öffnet ein Eingabefensterx für den Nutzer und speichert die Eingabe
-            max_hr_bpm  = input("Enter maximum heart rate:")
-        return int(max_hr_bpm)
-    
+            return 220 - self.age  # Fallback estimation, kann durch eine detailliertere Logik ersetzt werden
+
 class Experiment:
     def __init__(self, experiment_name, date, supervisor):
         self.experiment_name = experiment_name
@@ -32,9 +39,24 @@ class Experiment:
         data = self.__dict__
         with open(filename, 'w') as file:
             json.dump(data, file)
+    
+    def dictionary(self):
+        return {
+            'experiment_name': self.experiment_name,
+            'date': self.date,
+            'supervisor': self.supervisor.dictionary()
+        }
 
+if __name__ == '__main__':
+    test_subject = Subject('Sila', 'Erdogan', 21, 'female')
+    max_hr_bpm = test_subject.estimate_max_hr()
+    print('Max Heart Rate (bpm):', max_hr_bpm)
+    print('Subject Data:')
+    print(json.dumps(test_subject.dictionary(), indent=4))
 
-if __name__ == "__main__":
-    test_person = Person("Lukas ", "Balog", 21, "male")
-    max_hr_bpm = test_person.estimate_max_hr()
-    print("Max Heart Rate (bpm):", max_hr_bpm)
+    #Daten für den Supervisor
+    supervisor = Person('Lukas', 'Balog', 21, 'male')
+    
+    test_experiment = Experiment('Test_1', '26.04.2024', supervisor)
+    print('Experiment Data:')
+    print(json.dumps(test_experiment.dictionary(), indent=4))
